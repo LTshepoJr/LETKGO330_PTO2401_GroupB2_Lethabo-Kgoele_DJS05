2589abe85419f4559349c1781d9b81bf4f199645
Lethabo Kgoele <lethabotshepojr@gmail.com>
2024-09-23 11:56:54 +0200

    refined the observer

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 function tallyCreator() {
+  let tallyArray = [];
   let count = {
     value: 0,
   };
   return {
-    dispatch(action) {
-      switch (action) {
-        case "ADD":
-          return count++;
-        case "SUBTRACT":
-          return count--;
-        case "RESET":
-          return (count = 0);
-      }
+    // method to add method to array
+    subscribe(method) {
+      tallyArray.push(method);
     },
+    // method to remove method from array
+    unsubscribe(method) {
+      tallyArray = tallyArray.filter((tally) => tally !== method);
+    },
+    notify() {
+      tallyArray.forEach((tally) => tally(count));
+    },
+    dispatch(action) {
+      if (action === "ADD") {
+        count.value++;
+      } else if (action === "SUBTRACT") {
+        count.value--;
+      } else if (action === "RESET") {
+        count.value = 0;
+      }
+      this.notify();
+    },
+    // logs out the current state
     getState() {
-      console.log(count);
+      console.log(count.value);
     },
   };
 }
-
-const tally = tallyCreator();
-tally.dispatch("ADD");
-tally.dispatch("ADD");
-tally.dispatch("SUBTRACT");
-tally.dispatch("RESET");
-tally.getState();
+let tallyOne = tallyCreator();
+//created another method to be added at the tally array as a notification when dispatch is called
+tallyOne.subscribe((count1) => {
+  console.log(`TallyKeeper:`, count1);
+});
+tallyOne.dispatch("ADD");
+tallyOne.dispatch("SUBTRACT");
+tallyOne.dispatch("SUBTRACT");
+tallyOne.dispatch("SUBTRACT");
+tallyOne.dispatch("SUBTRACT");
+tallyOne.getState();
